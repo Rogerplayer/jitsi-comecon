cf230034229ae1e05f8a1bea0df87ff08b47473b
Rogerplayer <rogerio.correia.c@gmail.com>
2020-08-04 19:15:37 -0300

    Revert "Revert "Alterando projeto para Angular""
    This reverts commit a7d64c1b45747d5b95e2ec79a59ad4fb080cbe30.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SalaComponent } from './sala/sala.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SalaComponent }, 
+  { path: ':sala', component: SalaComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SalaComponent } from './sala/sala.component';
+import { AppMaterialModule } from './modules/material/material.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalaComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
